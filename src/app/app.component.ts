a44b105e061087f426e23c7ddfaced70637e653d
guptaShantanu <officialshantanugupta08@gmail.com>
2020-12-04 14:35:03 +0530

    Shantanu gupta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zazzy';
+  values = '';
+  colorValue
+
+  onInput(event: any) {
+    this.values = event.target.value;
+  }
+
+  onColorInput(event: any) {
+    var targets = ['a', 'an', 'the', 'he', 'she', 'they'];
+    var input = event.target.value;
+    var words = input.split(" ");
+    var box = document.getElementById('colorText')
+    box.innerHTML = "";
+    for (var word of words) {
+      if (word === 'the' || word === 'an' || word === 'a' || word === 'he' || word === 'she' || word === 'they') {
+        var boldText = document.createElement('strong');
+        boldText.innerHTML = word;
+        box.appendChild(boldText);
+      } else {
+        box.appendChild(document.createTextNode(word + ' '));
+      }
+    }
+
+  }
 }
